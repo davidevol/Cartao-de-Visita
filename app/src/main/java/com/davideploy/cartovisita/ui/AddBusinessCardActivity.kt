38c846c3ceb84a8460191b82828bbaf12b437e09
davideploy <deehzi@pm.me>
2022-08-05 12:52:05 -0300

    configurando as interfaces do app

--- app/src/main/java/com/davideploy/cartovisita/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/com/davideploy/cartovisita/ui/AddBusinessCardActivity.kt
@@ -2,15 +2,16 @@ package com.davideploy.cartovisita.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.davideploy.cartovisita.R
-import com.davideploy.cartovisita.databinding.ActivityMainBinding
+import com.davideploy.cartovisita.databinding.ActivityAddBusinessCardBinding
 
-class MainActivity : AppCompatActivity() {
 
-    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+class AddBusinessCardActivity : AppCompatActivity() {
+
+
+    private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
     }
 }
